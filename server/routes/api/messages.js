fbc0fed576885b6211aee887640fe410a0c7a91f
steve <kkingya@gmail.com>
2021-07-28 12:21:49 -0400

    fixed the bugs on first issue

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -12,12 +12,19 @@ router.post("/", async (req, res, next) => {
     const { recipientId, text, conversationId, sender } = req.body;
 
     // if we already know conversation id, we can save time and just add it to message and return
-
-    //I think this is the bug because you could potentially send post req to some random conversationId
-    // if (conversationId) {
-    //   const message = await Message.create({ senderId, text, conversationId });
-    //   return res.json({ message, sender });
-    // }
+    if (conversationId) {
+      const conv = await Conversation.findOne({
+        where: { id: conversationId },
+      });
+      if (conv.user1Id === senderId || conv.user2Id === senderId) {
+        const message = await Message.create({
+          senderId,
+          text,
+          conversationId,
+        });
+        return res.json({ message, sender });
+      }
+    }
 
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
